89eb5992fcba5a8989f18bd95f62d4208e929f89
Krelex <filip.cog@gmail.com>
2018-05-27 15:20:50 +0200

    Add and modifed grafikon.view (winForms)

---
+++ Grafikon.View/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Grafikon.View
+{
+    partial class Satnica
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.textBoxIme = new System.Windows.Forms.TextBox();
+            this.labelIme = new System.Windows.Forms.Label();
+            this.labelPrezime = new System.Windows.Forms.Label();
+            this.textBoxPrezime = new System.Windows.Forms.TextBox();
+            this.labelGodina = new System.Windows.Forms.Label();
+            this.textBoxGodina = new System.Windows.Forms.TextBox();
+            this.labelMjesec = new System.Windows.Forms.Label();
+            this.textBoxMjesec = new System.Windows.Forms.TextBox();
+            this.labelRadnoV = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.labelRadnoV2 = new System.Windows.Forms.Label();
+            this.labelPorodiljni = new System.Windows.Forms.Label();
+            this.radioButtonR = new System.Windows.Forms.RadioButton();
+            this.radioButtonR2 = new System.Windows.Forms.RadioButton();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.labelFileName = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(359, 458);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(149, 64);
+            this.btn1.TabIndex = 9;
+            this.btn1.Text = "DONE";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // textBoxIme
+            // 
+            this.textBoxIme.Location = new System.Drawing.Point(149, 130);
+            this.textBoxIme.Name = "textBoxIme";
+            this.textBoxIme.Size = new System.Drawing.Size(242, 26);
+            this.textBoxIme.TabIndex = 1;
+            // 
+            // labelIme
+            // 
+            this.labelIme.AutoSize = true;
+            this.labelIme.Location = new System.Drawing.Point(52, 136);
+            this.labelIme.Name = "labelIme";
+            this.labelIme.Size = new System.Drawing.Size(44, 20);
+            this.labelIme.TabIndex = 2;
+            this.labelIme.Text = "Ime :";
+            // 
+            // labelPrezime
+            // 
+            this.labelPrezime.AutoSize = true;
+            this.labelPrezime.Location = new System.Drawing.Point(52, 188);
+            this.labelPrezime.Name = "labelPrezime";
+            this.labelPrezime.Size = new System.Drawing.Size(74, 20);
+            this.labelPrezime.TabIndex = 4;
+            this.labelPrezime.Text = "Prezime :";
+            // 
+            // textBoxPrezime
+            // 
+            this.textBoxPrezime.Location = new System.Drawing.Point(149, 182);
+            this.textBoxPrezime.Name = "textBoxPrezime";
+            this.textBoxPrezime.Size = new System.Drawing.Size(242, 26);
+            this.textBoxPrezime.TabIndex = 2;
+            // 
+            // labelGodina
+            // 
+            this.labelGodina.AutoSize = true;
+            this.labelGodina.Location = new System.Drawing.Point(50, 237);
+            this.labelGodina.Name = "labelGodina";
+            this.labelGodina.Size = new System.Drawing.Size(69, 20);
+            this.labelGodina.TabIndex = 6;
+            this.labelGodina.Text = "Godina :";
+            // 
+            // textBoxGodina
+            // 
+            this.textBoxGodina.Location = new System.Drawing.Point(149, 231);
+            this.textBoxGodina.Name = "textBoxGodina";
+            this.textBoxGodina.Size = new System.Drawing.Size(242, 26);
+            this.textBoxGodina.TabIndex = 3;
+            // 
+            // labelMjesec
+            // 
+            this.labelMjesec.AutoSize = true;
+            this.labelMjesec.Location = new System.Drawing.Point(52, 287);
+            this.labelMjesec.Name = "labelMjesec";
+            this.labelMjesec.Size = new System.Drawing.Size(67, 20);
+            this.labelMjesec.TabIndex = 8;
+            this.labelMjesec.Text = "Mjesec :";
+            // 
+            // textBoxMjesec
+            // 
+            this.textBoxMjesec.Location = new System.Drawing.Point(149, 281);
+            this.textBoxMjesec.Name = "textBoxMjesec";
+            this.textBoxMjesec.Size = new System.Drawing.Size(242, 26);
+            this.textBoxMjesec.TabIndex = 4;
+            // 
+            // labelRadnoV
+            // 
+            this.labelRadnoV.AutoSize = true;
+            this.labelRadnoV.Location = new System.Drawing.Point(52, 337);
+            this.labelRadnoV.Name = "labelRadnoV";
+            this.labelRadnoV.Size = new System.Drawing.Size(150, 20);
+            this.labelRadnoV.TabIndex = 9;
+            this.labelRadnoV.Text = "Radno vrijeme : OD ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(208, 331);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(67, 26);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(324, 331);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(67, 26);
+            this.textBox2.TabIndex = 6;
+            // 
+            // labelRadnoV2
+            // 
+            this.labelRadnoV2.AutoSize = true;
+            this.labelRadnoV2.Location = new System.Drawing.Point(281, 337);
+            this.labelRadnoV2.Name = "labelRadnoV2";
+            this.labelRadnoV2.Size = new System.Drawing.Size(33, 20);
+            this.labelRadnoV2.TabIndex = 12;
+            this.labelRadnoV2.Text = "DO";
+            // 
+            // labelPorodiljni
+            // 
+            this.labelPorodiljni.AutoSize = true;
+            this.labelPorodiljni.Location = new System.Drawing.Point(52, 390);
+            this.labelPorodiljni.Name = "labelPorodiljni";
+            this.labelPorodiljni.Size = new System.Drawing.Size(134, 20);
+            this.labelPorodiljni.TabIndex = 13;
+            this.labelPorodiljni.Text = "Roditeljski dopust";
+            // 
+            // radioButtonR
+            // 
+            this.radioButtonR.AutoSize = true;
+            this.radioButtonR.Checked = true;
+            this.radioButtonR.Location = new System.Drawing.Point(219, 390);
+            this.radioButtonR.Name = "radioButtonR";
+            this.radioButtonR.Size = new System.Drawing.Size(56, 24);
+            this.radioButtonR.TabIndex = 7;
+            this.radioButtonR.TabStop = true;
+            this.radioButtonR.Text = "NE";
+            this.radioButtonR.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonR2
+            // 
+            this.radioButtonR2.AutoSize = true;
+            this.radioButtonR2.Location = new System.Drawing.Point(324, 390);
+            this.radioButtonR2.Name = "radioButtonR2";
+            this.radioButtonR2.Size = new System.Drawing.Size(57, 24);
+            this.radioButtonR2.TabIndex = 8;
+            this.radioButtonR2.Text = "DA";
+            this.radioButtonR2.UseVisualStyleBackColor = true;
+            // 
+            // labelPath
+            // 
+            this.labelPath.AutoSize = true;
+            this.labelPath.Location = new System.Drawing.Point(451, 136);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(90, 20);
+            this.labelPath.TabIndex = 16;
+            this.labelPath.Text = "Save Path :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(455, 182);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(370, 26);
+            this.textBox3.TabIndex = 17;
+            // 
+            // labelFileName
+            // 
+            this.labelFileName.AutoSize = true;
+            this.labelFileName.Location = new System.Drawing.Point(451, 237);
+            this.labelFileName.Name = "labelFileName";
+            this.labelFileName.Size = new System.Drawing.Size(86, 20);
+            this.labelFileName.TabIndex = 18;
+            this.labelFileName.Text = "File name :";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(573, 234);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(252, 26);
+            this.textBox4.TabIndex = 19;
+            // 
+            // Satnica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 566);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.labelFileName);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.labelPath);
+            this.Controls.Add(this.radioButtonR2);
+            this.Controls.Add(this.radioButtonR);
+            this.Controls.Add(this.labelPorodiljni);
+            this.Controls.Add(this.labelRadnoV2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelRadnoV);
+            this.Controls.Add(this.labelMjesec);
+            this.Controls.Add(this.textBoxMjesec);
+            this.Controls.Add(this.labelGodina);
+            this.Controls.Add(this.textBoxGodina);
+            this.Controls.Add(this.labelPrezime);
+            this.Controls.Add(this.textBoxPrezime);
+            this.Controls.Add(this.labelIme);
+            this.Controls.Add(this.textBoxIme);
+            this.Controls.Add(this.btn1);
+            this.Name = "Satnica";
+            this.Text = "Satnica input";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox textBoxIme;
+        private System.Windows.Forms.Label labelIme;
+        private System.Windows.Forms.Label labelPrezime;
+        private System.Windows.Forms.TextBox textBoxPrezime;
+        private System.Windows.Forms.Label labelGodina;
+        private System.Windows.Forms.TextBox textBoxGodina;
+        private System.Windows.Forms.Label labelMjesec;
+        private System.Windows.Forms.TextBox textBoxMjesec;
+        private System.Windows.Forms.Label labelRadnoV;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label labelRadnoV2;
+        private System.Windows.Forms.Label labelPorodiljni;
+        private System.Windows.Forms.RadioButton radioButtonR;
+        private System.Windows.Forms.RadioButton radioButtonR2;
+        private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label labelFileName;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
